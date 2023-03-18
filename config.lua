95a373bca7ed75a942f0dc1c9624aff5cc7046b5
yoeltripp <cris.ct2003@gmail.com>
2023-03-18 18:34:50 -0300

    first locations ready

--- config.lua
+++ config.lua
@@ -1,1 +1,20 @@
 Config = {}
+
+Config.Ped = {
+    Name = "a_m_y_business_03",
+    Pos = {x = 166.8932, y = -1284.169, z = 28.35997, h = 80.51255}
+    
+}
+
+Config.Houses = {
+    House1 = {
+        Door = vector3(-174.7202, 502.5304, 137.4204),
+        Interior = vector3(-174.3378, 497.484, 137.6535),
+        HeadingInt = 191.0619
+    }
+}
+Config.OxTarget = true
+Config.JobName = 'police'
+
+Config.SkillDifficulty = 'medium' -- easy, medium, hard
+Config.SkillRepeatTimes =4 -- How many times the skill check repeats until finish
