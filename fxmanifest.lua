3cd1487c691371e0e8b89ffdc17541157bcda05b
yoeltripp <cris.ct2003@gmail.com>
2023-03-18 18:34:57 -0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,15 @@
 fx_version 'cerulean'
 games { 'gta5' }
 author 'Fly Development'
-
-
+lua54 'yes'
 server_scripts {
      'server.lua'
 } 
 client_scripts {
      'client.lua',
      'config.lua'
+}
+
+shared_scripts {
+    '@ox_lib/init.lua'
 }
