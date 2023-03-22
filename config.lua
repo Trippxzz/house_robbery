e27f8dc4bad750c4b252b214611647cc1cc64fda
yoeltripp <cris.ct2003@gmail.com>
2023-03-21 22:18:12 -0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,16 @@
 Config = {}
 
+
+Config.OxTarget = true
+Config.JobName = 'police'
+
+Config.RequiredItem = true
+Config.ItemRequired = "lockpick"
+Config.BlipEnabled = true
+Config.CopsRequired = 0
+Config.Cooldown = 180 
+Config.Lastrob = 0 -- DON'T TOUCH
+
 Config.Ped = {
     Name = "a_m_y_business_03",
     Pos = {x = 166.8932, y = -1284.169, z = 28.35997, h = 80.51255}
@@ -17,12 +28,48 @@ Config.Houses = {
             vector3(-167.5402, 488.0954, 133.8438),
             vector3(-174.4345, 493.9098, 130.0436)
         }
+    },
+    House2 = { 
+        Door = vector3(-36.30156, -570.6331, 38.83335), 
+        Interior = vector3(-31.4882, -595.133, 80.03), 
+        HeadingInt = 311.318,
+        LootH = {
+            vector3(-27.74033, -581.5154, 79.23),  
+            vector3(-39.19651, -589.2886, 78.83),
+            vector3(-32.36621, -583.7902, 78.86551), 
+            vector3(-12.62623, -596.9193, 79.43) 
+        }
+    },
+    House3 = { 
+        Door = vector3(-686.044, 596.1514, 143.6422),
+        Interior = vector3(-682.4269, 592.7078, 145.37),  
+        HeadingInt = 224.9117,  --heading for interior 
+        LootH = {
+            vector3(-678.1968, 593.1891, 145.3798),   
+            vector3(-671.786, 581.1215, 144.9703), 
+            vector3(-671.7256, 587.5762, 141.5699),  
+            vector3(-680.682, 589.1367, 137.7697)  
+        }
     }
 }
-Config.OxTarget = true
-Config.JobName = 'police'
 
-Config.SkillCheck = {
-    difficulty = 'hard', -- easy, medium, hard
-    repeatTimes = math.random(5, 10), -- How many times the skill check repeats until finish
+Config.Loot = {
+    badloot = {
+        "bread",
+        "phone"
+    },
+    mediumloot = {
+        "diamond",
+        "weapon_pistol"
+    },
+    goodloot = {
+        "money",
+        "black_money"
+    }
+}
+
+Config.Cant = {
+    badloot = {1, 2},
+    mediumloot = {1, 4},
+    goodloot = {1000, 10000} --For money
 }
