a453198adbc12430cb64fdd3a54b1024c36225a5
yoeltripp <cris.ct2003@gmail.com>
2023-03-25 17:07:58 -0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -7,7 +7,7 @@ Config.JobName = 'police'
 Config.RequiredItem = true
 Config.ItemRequired = "lockpick"
 Config.BlipEnabled = true
-Config.CopsRequired = 0
+Config.CopsRequired = 2
 Config.Cooldown = 180 
 Config.Lastrob = 0 -- DON'T TOUCH
 
@@ -18,10 +18,10 @@ Config.Ped = {
 }
 
 Config.Houses = {
-    House1 = {
+    House1 = {   --you can add as many locations as you want
         Door = vector3(-174.7202, 502.5304, 137.4204),
         Interior = vector3(-174.3378, 497.484, 137.6535), 
-        HeadingInt = 191.0619,
+        HeadingInt = 191.0619,  --heading for interior 
         LootH = {
             vector3(-170.3029, 496.1262, 137.6536),
             vector3(-170.6799, 482.5876, 137.2442),
@@ -43,7 +43,7 @@ Config.Houses = {
     House3 = { 
         Door = vector3(-686.044, 596.1514, 143.6422),
         Interior = vector3(-682.4269, 592.7078, 145.37),  
-        HeadingInt = 224.9117,  --heading for interior 
+        HeadingInt = 224.9117,  
         LootH = {
             vector3(-678.1968, 593.1891, 145.3798),   
             vector3(-671.786, 581.1215, 144.9703), 
@@ -70,6 +70,6 @@ Config.Loot = {
 
 Config.Cant = {
     badloot = {1, 2},
-    mediumloot = {1, 4},
+    mediumloot = {1, 2},
     goodloot = {1000, 10000} --For money
 }
